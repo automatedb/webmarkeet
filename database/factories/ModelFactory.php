f5aa99452b386678761024ff50c25584bcba9c13
automatedb <contact@robots-trading.com>
2017-07-23 09:48:42 +0200

    issue #15 - Intégration des thumbnails dans les contenus
    - Mise à jour de la factory pour la table contents
    - Mise à jour des vues
    - Ajout des images du dossier public dans le gitignore

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,6 +32,10 @@ $factory->define(App\Models\Content::class, function(Faker\Generator $faker): ar
         \App\Models\Content::$SLUG => str_slug($title),
         \App\Models\Content::$CONTENT => $faker->paragraphs(3, true),
         \App\Models\Content::$STATUS => $faker->word,
-        \App\Models\Content::$THUMBNAIL => $faker->url
+        \App\Models\Content::$THUMBNAIL => array_random([
+            '/img/01.jpg',
+            '/img/02.jpg',
+            '/img/03.jpg'
+        ])
     ];
 });
--- resources/views/Content/index.blade.php
+++ resources/views/Content/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     @foreach ($contents as $content)
+        <img src="{{ $content->thumbnail }}" alt="">
         <h1><a href="/blog/{{ $content->slug }}">{{ $content->title }}</a></h1>
         <p>{{ str_limit($content->content, 200) }}</p>
     @endforeach
