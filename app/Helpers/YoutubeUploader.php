d94a307b09750d5e07544aa3e4c2d732f17f3277
nico-develop <contact@nico-develop.fr>
2018-02-27 10:11:27 +0100

    Permet de différencier l'environnement de production de celle de dev

--- app/Helpers/YoutubeUploader.php
+++ app/Helpers/YoutubeUploader.php
@@ -14,12 +14,21 @@ use Dawson\Youtube\Facades\Youtube;
 class YoutubeUploader
 {
     public function upload(string $filename, string $title, string $description, array $tags = [], string $thumbnail = ""): string {
-        $video = Youtube::upload(storage_path($filename), [
-            'title'       => $title,
-            'description' => $description,
-            'tags'	      => $tags,
-            'category_id' => 27
-        ]);
+        if(config('app.env') === 'production') {
+            $video = Youtube::upload(storage_path($filename), [
+                'title'       => $title,
+                'description' => $description,
+                'tags'	      => $tags,
+                'category_id' => 27
+            ]);
+        } else {
+            $video = Youtube::upload(storage_path($filename), [
+                'title'       => $title,
+                'description' => $description,
+                'tags'	      => $tags,
+                'category_id' => 27
+            ], 'unlisted');
+        }
 
         return $video->getVideoId();
     }
